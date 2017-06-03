2f493c0d94f327a03256ecb57d3acffe8090c8c0
Carlos Godino <cgodino@gmail.com>
2017-06-03 14:26:04 -0700

    Python Belt Exam Finish

--- PythonExamProj/apps/exam/urls.py
+++ PythonExamProj/apps/exam/urls.py
@@ -4,4 +4,11 @@ from . import views
 app_name = 'exam'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^travels$', views.travels, name='travels'),
+    url(r'^travels/destination/(?P<trip_id>\d+)$', views.destination, name='destination'),
+    url(r'^travels/add$', views.addtravel, name='addtravel'),
+    url(r'^checktravel$', views.checktravel, name='checktravel'),
+    url(r'^checkuser$', views.checkuser, name='checkuser'),
+    url(r'^logout$', views.logout, name='logout'),
+    url(r'^jointravel/(?P<trip_id>\d+)$', views.jointravel, name='jointravel'),
 ]
